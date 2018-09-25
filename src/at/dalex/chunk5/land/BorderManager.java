1147df7e49d0addd6aacd8c0c55880a34d9be94f
Niki Kraus <nikitelli@gmail.com>
2018-09-25 08:00:30 +0200

    Added LandManager and BorderManager

---
+++ src/at/dalex/chunk5/land/BorderManager.java
@@ -0,0 +1,93 @@
+package at.dalex.chunk5.manager;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class BorderManager implements Runnable {
+
+    private Plugin plugin;
+    private Chunk chunk;
+    private Material torchMaterial;
+    private TorchPlaceWork work;
+
+    public BorderManager(Plugin plugin, Chunk chunk, Material torchMaterial) {
+        this.plugin = plugin;
+        this.chunk = chunk;
+        this.torchMaterial = torchMaterial;
+
+        this.work = new TorchPlaceWork();
+    }
+
+    @Override
+    public void run() {
+
+        if (this.work.isWorkDone()) {
+            return;
+        }
+
+        for (int count = 0; (count < 4) && (!this.work.isWorkDone()); count++) {
+            Location block1 = this.chunk.getBlock(this.work.i, 0, 0).getLocation();
+            block1.setY(getY(this.chunk.getWorld(), block1.getBlockX(), block1.getBlockZ()));
+            this.chunk.getWorld().getBlockAt(block1).setType(this.torchMaterial);
+
+            Location block2 = this.chunk.getBlock(15, 0, this.work.i).getLocation();
+            block2.setY(getY(this.chunk.getWorld(), block2.getBlockX(), block2.getBlockZ()));
+            this.chunk.getWorld().getBlockAt(block2).setType(this.torchMaterial);
+
+            Location block3 = this.chunk.getBlock(15 - this.work.i, 0, 15).getLocation();
+            block3.setY(getY(this.chunk.getWorld(), block3.getBlockX(), block3.getBlockZ()));
+            this.chunk.getWorld().getBlockAt(block3).setType(this.torchMaterial);
+
+            Location block4 = this.chunk.getBlock(0, 0, 15 - this.work.i).getLocation();
+            block4.setY(getY(this.chunk.getWorld(), block4.getBlockX(), block4.getBlockZ()));
+            this.chunk.getWorld().getBlockAt(block4).setType(this.torchMaterial);
+
+            this.work.i += 1;
+        }
+        if (!this.work.isWorkDone()) {
+            this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, this);
+        }
+    }
+
+    public int getY(World world, int x, int z) {
+        int y2 = 0;
+        for (int y = 255; y > 0; y--) {
+            if ((world.getBlockAt(x, y, z).getType() != Material.AIR) &&
+                    (world.getBlockAt(x, y, z).getType() != Material.ENDER_PORTAL_FRAME) &&
+                    (world.getBlockAt(x, y, z).getType() != Material.BEDROCK)) {
+
+                if ((world.getBlockAt(x, y, z).getType() != Material.DEAD_BUSH) && (world.getBlockAt(x, y, z).getType() != Material.SNOW)) {
+                    if (world.getBlockAt(x, y, z).getType() == Material.LONG_GRASS) {
+                        y2 = y - 1;
+                        break;
+                    }
+                    if (world.getBlockAt(x, y, z).getType() == Material.DOUBLE_PLANT) {
+                        y2 = y - 2;
+                        break;
+                    }
+                    y2 = y;
+                    break;
+                }
+                y--;
+                break;
+            }
+        }
+        return y2 + 1;
+    }
+
+    public class TorchPlaceWork {
+        public int i = 0;
+
+        public TorchPlaceWork() {}
+
+        public boolean isWorkDone() {
+            return this.i == 15;
+        }
+    }
+}
---
+++ src/at/dalex/chunk5/land/LandManager.java
@@ -0,0 +1,104 @@
+package at.dalex.chunk5.land;
+
+import at.dalex.chunk5.config.Config;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+public class LandManager {
+    private ArrayList<Land> lands = new ArrayList();
+    private ArrayList<UUID> landIds = new ArrayList();
+
+    public boolean addLand(Land land) {
+        if (!this.lands.contains(land)) {
+            this.lands.add(land);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeLand(Land land) {
+        if (this.lands.contains(land)) {
+            this.lands.remove(land);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isLand(Chunk chunk) {
+        for (Land land : this.lands) {
+            if (land.getData().chunk.equals(chunk)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasRightOn(Chunk chunk, Player p) {
+        if (!p.getWorld().getName().equals(Config.spawn.getWorld().getName())) {
+            return true;
+        }
+        if (isBoughtByPlayer(chunk, p)) {
+            return true;
+        }
+        if ((getLand(chunk) != null) && (getLand(chunk).getFriends() != null) && (getLand(chunk).getFriends().contains(p.getUniqueId()))) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBoughtByPlayer(Chunk chunk, Player p) {
+        for (Land land : this.lands) {
+            if (land.getData().chunk.equals(chunk))
+            {
+                if (land.getData().landOwner.equals(p.getUniqueId())) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public Land getLand(Chunk chunk) {
+        for (Land land : this.lands) {
+            if (land.getData().chunk.equals(chunk)) {
+                return land;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Land> getRegisteredLands() {
+        return this.lands;
+    }
+
+    public ArrayList<UUID> getRegisteredUUIDs() {
+        ArrayList<UUID> list = new ArrayList();
+        for (Land land : this.lands) {
+            list.add(land.getData().landOwner);
+        }
+        return list;
+    }
+
+    public UUID genLandId() {
+        UUID landId = null;
+        boolean done = false;
+        while (!done)
+        {
+            landId = UUID.randomUUID();
+            if (!this.landIds.contains(landId)) {
+                done = true;
+            }
+        }
+        return landId;
+    }
+
+    public void reset() {
+        this.lands.clear();
+        this.landIds.clear();
+    }
+}
